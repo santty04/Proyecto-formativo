7a01af48405966281bf93b79c1a20c67fbcd4283
castridani <castridani7@gmail.com>
2022-11-28 15:19:26 -0500

    backup

--- proyect-form/backup/liceo_aprender.sql
+++ proyect-form/backup/liceo_aprender.sql
@@ -2,10 +2,10 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Nov 17, 2022 at 07:36 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 28-11-2022 a las 13:31:16
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `liceo_aprender`
+-- Base de datos: `liceo_aprender`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `acudiente`
+-- Estructura de tabla para la tabla `acudiente`
 --
 
 CREATE TABLE `acudiente` (
@@ -40,20 +40,27 @@ CREATE TABLE `acudiente` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `detalles`
+-- Estructura de tabla para la tabla `detalles`
 --
 
 CREATE TABLE `detalles` (
   `id` int(11) NOT NULL,
   `pago_id` int(11) DEFAULT NULL,
   `valor` int(11) DEFAULT NULL,
-  `mes` varchar(25) DEFAULT NULL
+  `mes` varchar(25) DEFAULT NULL,
+  `desayuno` int(32) NOT NULL,
+  `media_mañana` int(32) NOT NULL,
+  `media_tarde` int(32) NOT NULL,
+  `almuerzo` int(32) NOT NULL,
+  `transporte` int(32) NOT NULL,
+  `derecho_grado` int(32) NOT NULL,
+  `matricula` int(32) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `estudiantes`
+-- Estructura de tabla para la tabla `estudiantes`
 --
 
 CREATE TABLE `estudiantes` (
@@ -70,31 +77,34 @@ CREATE TABLE `estudiantes` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pagos`
+-- Estructura de tabla para la tabla `pagos`
 --
 
 CREATE TABLE `pagos` (
   `factura_id` int(11) NOT NULL,
   `fecha` date DEFAULT NULL,
-  `estudiantes_id` int(11) DEFAULT NULL
+  `mes` varchar(32) NOT NULL,
+  `pension` int(11) NOT NULL,
+  `num_recibo_manual` int(11) NOT NULL,
+  `estudiantes_id` int(11) DEFAULT NULL,
+  `id_tipo_pago` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tipo_pago`
+-- Estructura de tabla para la tabla `tipo_pago`
 --
 
 CREATE TABLE `tipo_pago` (
   `id` int(11) NOT NULL,
-  `detalles_id` int(11) DEFAULT NULL,
-  `tipo_pago` varchar(25) DEFAULT NULL
+  `detalles_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `usuarios`
+-- Estructura de tabla para la tabla `usuarios`
 --
 
 CREATE TABLE `usuarios` (
@@ -107,7 +117,7 @@ CREATE TABLE `usuarios` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `usuarios`
+-- Volcado de datos para la tabla `usuarios`
 --
 
 INSERT INTO `usuarios` (`id`, `name`, `email`, `photo`, `password`, `role`) VALUES
@@ -115,113 +125,115 @@ INSERT INTO `usuarios` (`id`, `name`, `email`, `photo`, `password`, `role`) VALU
 (7, 'Fernando Gaviria', 'fernando.g@gmail.com', 'public/images/1667574388.', '827ccb0eea8a706c4c34a16891f84e7b', 'Rector');
 
 --
--- Indexes for dumped tables
+-- Índices para tablas volcadas
 --
 
 --
--- Indexes for table `acudiente`
+-- Indices de la tabla `acudiente`
 --
 ALTER TABLE `acudiente`
   ADD PRIMARY KEY (`id`),
   ADD KEY `id_estudiante` (`id_estudiante`);
 
 --
--- Indexes for table `detalles`
+-- Indices de la tabla `detalles`
 --
 ALTER TABLE `detalles`
   ADD PRIMARY KEY (`id`),
   ADD KEY `pago_id` (`pago_id`);
 
 --
--- Indexes for table `estudiantes`
+-- Indices de la tabla `estudiantes`
 --
 ALTER TABLE `estudiantes`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `pagos`
+-- Indices de la tabla `pagos`
 --
 ALTER TABLE `pagos`
   ADD PRIMARY KEY (`factura_id`),
-  ADD KEY `estudiantes_id` (`estudiantes_id`);
+  ADD KEY `estudiantes_id` (`estudiantes_id`),
+  ADD KEY `id_tipo_pago` (`id_tipo_pago`);
 
 --
--- Indexes for table `tipo_pago`
+-- Indices de la tabla `tipo_pago`
 --
 ALTER TABLE `tipo_pago`
   ADD PRIMARY KEY (`id`),
   ADD KEY `detalles_id` (`detalles_id`);
 
 --
--- Indexes for table `usuarios`
+-- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT for table `acudiente`
+-- AUTO_INCREMENT de la tabla `acudiente`
 --
 ALTER TABLE `acudiente`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `detalles`
+-- AUTO_INCREMENT de la tabla `detalles`
 --
 ALTER TABLE `detalles`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `estudiantes`
+-- AUTO_INCREMENT de la tabla `estudiantes`
 --
 ALTER TABLE `estudiantes`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `pagos`
+-- AUTO_INCREMENT de la tabla `pagos`
 --
 ALTER TABLE `pagos`
   MODIFY `factura_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `tipo_pago`
+-- AUTO_INCREMENT de la tabla `tipo_pago`
 --
 ALTER TABLE `tipo_pago`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `usuarios`
+-- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- Constraints for dumped tables
+-- Restricciones para tablas volcadas
 --
 
 --
--- Constraints for table `acudiente`
+-- Filtros para la tabla `acudiente`
 --
 ALTER TABLE `acudiente`
   ADD CONSTRAINT `acudiente_ibfk_1` FOREIGN KEY (`id_estudiante`) REFERENCES `estudiantes` (`id`);
 
 --
--- Constraints for table `detalles`
+-- Filtros para la tabla `detalles`
 --
 ALTER TABLE `detalles`
   ADD CONSTRAINT `detalles_ibfk_1` FOREIGN KEY (`pago_id`) REFERENCES `pagos` (`factura_id`);
 
 --
--- Constraints for table `pagos`
+-- Filtros para la tabla `pagos`
 --
 ALTER TABLE `pagos`
-  ADD CONSTRAINT `pagos_ibfk_1` FOREIGN KEY (`estudiantes_id`) REFERENCES `estudiantes` (`id`);
+  ADD CONSTRAINT `pagos_ibfk_1` FOREIGN KEY (`estudiantes_id`) REFERENCES `estudiantes` (`id`),
+  ADD CONSTRAINT `pagos_ibfk_2` FOREIGN KEY (`id_tipo_pago`) REFERENCES `tipo_pago` (`id`);
 
 --
--- Constraints for table `tipo_pago`
+-- Filtros para la tabla `tipo_pago`
 --
 ALTER TABLE `tipo_pago`
   ADD CONSTRAINT `tipo_pago_ibfk_1` FOREIGN KEY (`detalles_id`) REFERENCES `pagos` (`factura_id`);
